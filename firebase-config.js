0b1020f309b45f38bc271e3955fbe129f2b3ece8
Bennyjmiller23 <bennyjmiller@gmail.com>
2026-08-18 17:57:06 -0700

    Add Firebase configuration

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD2Qsd1_W7fldcPBqTXGjA9Ex130lCmoeo",
+  authDomain: "paper-trails-portland.firebaseapp.com",
+  projectId: "paper-trails-portland",
+  storageBucket: "paper-trails-portland.firebasestorage.app",
+  messagingSenderId: "23923120383",
+  appId: "1:23923120383:web:10cd19122983a1d86b94ca"
+};
